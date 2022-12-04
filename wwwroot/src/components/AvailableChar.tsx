ade2f8dff66dee49046e2d1e40c71739fb554c86
mkaraki <masakazu.araki@mkarakiapps.com>
2022-12-05 07:29:18 +0900

    Move website codes to https://github.com/sakamata-ch/SakamataFontProject-Website

--- wwwroot/src/components/AvailableChar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function AvailableChar({ highlight, string }: { highlight: Boolean | null, string: string }): JSX.Element {
-    return (
-        <td style={{ backgroundColor: highlight !== null ? (highlight ? 'lightgreen' : 'lightpink') : '' }}>
-            {string}
-        </td>
-    );
-}
