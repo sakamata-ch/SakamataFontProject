4688a3b0789ae222e3f53e30c89d278a5691a9b6
mkaraki <masakazu.araki@mkarakiapps.com>
2023-01-28 22:03:02 +0900

    Make more bigger

--- pr-commenter/grid.py
+++ pr-commenter/grid.py
@@ -88,6 +88,7 @@ for c in cmap:
 
 pm = math.ceil(math.sqrt(len(imgs)))
 
+plt.figure(figsize=(30, 30), dpi=120)
 fig, ax = plt.subplots(pm * 2, pm, figsize=(10, 10))
 fig.subplots_adjust(hspace=0, wspace=0)
 
